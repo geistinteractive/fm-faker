c022580a19e700d2e77a60675a025afa98eb7165
Todd Geist <todd@geistinteractive.com>
2019-12-02 13:24:48 -0800

    added date range

--- utils/generateData/lib/fmDate.js
+++ utils/generateData/lib/fmDate.js
@@ -1,4 +1,11 @@
-import { format, addDays, subDays } from "date-fns";
+import {
+  format,
+  addDays,
+  subDays,
+  differenceInDays,
+  parse,
+  differenceInCalendarDays
+} from "date-fns";
 import { randomIntFromInterval } from "./utils";
 
 export default function fmDate(value, schema) {
@@ -7,6 +14,8 @@ export default function fmDate(value, schema) {
 
   if (value.relativeDays) {
     date = relativeDays(value.relativeDays);
+  } else if (value.range) {
+    date = dateRange(value.range.from, value.range.to, dateFormat);
   } else {
     // default for now
     date = relativeDays({});
@@ -16,6 +25,8 @@ export default function fmDate(value, schema) {
 }
 
 function relativeDays({ from = -500000, to = 50000 }) {
+  console.log(from, to);
+
   let d = new Date();
 
   if (from > to) {
@@ -24,6 +35,7 @@ function relativeDays({ from = -500000, to = 50000 }) {
   }
 
   const r = randomIntFromInterval(from, to);
+  console.log(r);
 
   if (d < 0) {
     d = subDays(d, r);
@@ -31,4 +43,19 @@ function relativeDays({ from = -500000, to = 50000 }) {
     d = addDays(d, r);
   }
   return d;
+}
+
+function dateRange(fromDateString, toDateString, format) {
+  try {
+    const fromDate = parse(fromDateString, format, new Date());
+    const toDate = parse(toDateString, format, new Date());
+
+    const now = new Date();
+    const from = differenceInCalendarDays(fromDate, now);
+    const to = differenceInCalendarDays(toDate, now);
+    return relativeDays({ from, to });
+  } catch (e) {
+    console.log(e.toString());
+    return null;
+  }
 }
