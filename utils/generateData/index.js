3a55ddb5d8b8aa5b85716e1af2145de55dcc8c35
Todd Geist <todd@geistinteractive.com>
2019-12-02 11:08:26 -0800

    refactor fm-date

--- utils/generateData/index.js
+++ utils/generateData/index.js
@@ -1,7 +1,7 @@
 import jsf from "json-schema-faker";
 import chance from "chance";
 import { format, addDays, subDays } from "date-fns";
-import fk from "faker";
+import fmDate from "./lib/fmDate";
 
 export default function generateData(schema) {
   jsf.define("fm-date", fmDate);
@@ -11,36 +11,3 @@ export default function generateData(schema) {
   return jsf.resolve(schema);
 }
 
-function fmDate(value, schema) {
-  let d = new Date();
-
-  let dateFormat = "MM/dd/yyyy";
-  let from = -10000;
-  let to = 10000;
-  if (value.from !== undefined) {
-    from = value.from;
-  }
-  if (value.to !== undefined) {
-    to = value.to;
-  }
-
-  if (from > to) {
-    from = -10000;
-    to = 10000;
-  }
-  if (value.format) dateFormat = value.format;
-
-  const r = randomIntFromInterval(from, to);
-  if (d < 0) {
-    d = subDays(d, r);
-  } else {
-    d = addDays(d, r);
-  }
-
-  return format(d, dateFormat);
-}
-
-function randomIntFromInterval(min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
---
+++ utils/generateData/lib/fmDate.js
@@ -0,0 +1,34 @@
+import { format, addDays, subDays } from "date-fns";
+import { randomIntFromInterval } from "./utils";
+
+export default function fmDate(value, schema) {
+  let dateFormat = value.format ? value.format : "MM/dd/yyyy";
+  let date;
+
+  if (value.relativeDays) {
+    date = relativeDays(value.relativeDays);
+  } else {
+    // default for now
+    date = relativeDays();
+  }
+
+  return format(date, dateFormat);
+}
+
+function relativeDays({ from = -500000, to = 50000 }) {
+  let d = new Date();
+
+  if (from > to) {
+    from = -50000;
+    to = 50000;
+  }
+
+  const r = randomIntFromInterval(from, to);
+
+  if (d < 0) {
+    d = subDays(d, r);
+  } else {
+    d = addDays(d, r);
+  }
+  return d;
+}
