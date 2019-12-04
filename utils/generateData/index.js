74401e94680715531f1eae8b40d20356f1471652
Todd Geist <todd@geistinteractive.com>
2019-12-03 19:56:27 -0800

    add fm-time to generator

--- utils/generateData/index.js
+++ utils/generateData/index.js
@@ -1,13 +1,12 @@
 import jsf from "json-schema-faker";
 import chance from "chance";
-import { format, addDays, subDays } from "date-fns";
 import fmDate from "./lib/fmDate";
-
+import fmTime from "./lib/fmTime";
 export default function generateData(schema) {
   jsf.define("fm-date", fmDate);
+  jsf.define("fm-time", fmTime);
   jsf.extend("chance", chance);
   jsf.extend("faker", () => require("faker"));
   jsf.option({ resolveJsonPath: true });
   return jsf.resolve(schema);
 }
-
