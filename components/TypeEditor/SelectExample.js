7967b388eb2fd528e5166fb9027e8214451e61b1
Todd Geist <todd@geistinteractive.com>
2019-12-10 13:37:31 -0800

    Example Select Improved

--- components/TypeEditor/SelectExample.js
+++ components/TypeEditor/SelectExample.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Input } from "reactstrap";
 
-import examples from "./examples.json";
+import examples from "./examples";
 
 export default function SelectExample({ onSelect }) {
   const options = Object.keys(examples);
@@ -12,12 +12,20 @@ export default function SelectExample({ onSelect }) {
         type="select"
         onChange={ev => {
           const selection = ev.target.value;
-          const schema = examples[selection];
+          const schema = examples[selection].schema;
           onSelect(schema);
         }}
       >
         <option></option>
         {options.map(opt => {
+          const label = examples[opt].label;
+          if (label) {
+            return (
+              <option value={opt} key={opt}>
+                {label}
+              </option>
+            );
+          }
           return <option key={opt}>{opt}</option>;
         })}
       </Input>
---
+++ components/TypeEditor/examples/index.js
@@ -0,0 +1,5 @@
+import FMexamples from "./FMExamples.json";
+import ChanceExamples from "./Chance.json";
+import Faker from "./Faker.json";
+
+export default { ...FMexamples, ...ChanceExamples, ...Faker };
